6741c131779c1ec1051bb87db09056d5a007b070
QueenieeQ <xuanquyhnvn@gmail.com>
2023-04-13 15:13:31 +0700

    Breadth-first search

--- TimDuongNganNhat.playground/Contents.swift
+++ TimDuongNganNhat.playground/Contents.swift
@@ -1,3 +1,12 @@
 // BFS Breadth-first search tìm kiếm theo chiều rộng
 import Foundation
 
+
+struct Cell: Hashable {
+    let x: Int
+    let y: Int
+    
+    static func == (leftHandSide: Cell, rightHandSide: Cell) -> Bool {
+        return leftHandSide.x == rightHandSide.x && leftHandSide.y == rightHandSide.y
+    }
+}
