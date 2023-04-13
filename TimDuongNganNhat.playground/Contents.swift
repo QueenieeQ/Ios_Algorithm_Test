e79c048469843bc49b73e4f743b1d621d5c0b099
QueenieeQ <xuanquyhnvn@gmail.com>
2023-04-13 15:19:19 +0700

    Breadth-first search

--- TimDuongNganNhat.playground/Contents.swift
+++ TimDuongNganNhat.playground/Contents.swift
@@ -10,3 +10,12 @@ struct Cell: Hashable {
         return leftHandSide.x == rightHandSide.x && leftHandSide.y == rightHandSide.y
     }
 }
+func shortestPath(_ M: Int, _ N: Int, _ obstacles: [(Int, Int)], _ A: (Int, Int), _ B: (Int, Int)) -> Int {
+//    Tạo set các vật cản
+    let obstaclesSet = Set(obstacles.map(Cell.init))
+// Khởi tạo hàng chờ với điểm bắt đầu là A và lần lượt thăm các ô xung quanh, ghi chúng vào dictionary để so sánh. Set khoảng cách của A là 0
+    return 1
+    var q = [(Cell(x: A.0, y: A.1), 0)]
+        var visited = Set([Cell(x: A.0, y: A.1)])
+        
+}
